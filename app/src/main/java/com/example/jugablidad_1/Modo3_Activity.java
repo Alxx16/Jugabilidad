5a50a4a1fc9b5445bf9c47a592d33f2e5b2ca47b
Kovin2806 <kevin28enoc06@gmail.com>
2022-07-14 01:54:41 -0500

    AAA

--- app/src/main/java/com/example/jugablidad_1/Modo3_Activity.java
+++ app/src/main/java/com/example/jugablidad_1/Modo3_Activity.java
@@ -1,6 +1,7 @@
 package com.example.jugablidad_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -55,6 +56,7 @@ public class Modo3_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modo3);
+
         this.InicializarControles();
 
     }
